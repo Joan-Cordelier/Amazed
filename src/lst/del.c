cf5d3c95b0660acbb631010eaa4daa3b50ce09df
joan cordelier <joan.cordelier@epitech.eu>
2024-04-09 14:52:35 +0200

    FIX: segfault the segfault was if the first line have a le under 2

--- src/lst/del.c
+++ src/lst/del.c
@@ -12,14 +12,16 @@ int del(void *data)
     parsing_t **list = (parsing_t **) data;
     parsing_t *to_rm = NULL;
 
-    if ((*list)->str[0] == '#' && (*list)->str[1] != '#'){
+    if (my_strlen((*list)->str) > 1 &&
+        ((*list)->str[0] == '#' && (*list)->str[1] != '#')){
         to_rm = (*list)->next;
         free((*list)->str);
         free((*list));
         (*list) = to_rm;
     }
     for (parsing_t *tmp = *list; tmp->next != NULL; tmp = tmp->next) {
-        if (tmp->next->str[0] == '#' && tmp->next->str[1] != '#') {
+        if (my_strlen(tmp->next->str) > 1 &&
+            (tmp->next->str[0] == '#' && tmp->next->str[1] != '#')) {
             to_rm = tmp->next->next;
             free(tmp->next->str);
             free(tmp->next);
